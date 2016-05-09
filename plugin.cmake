ba7a20a133bd65caa55d96ff883be82ae6b7a7bc
Jonathan Beezley <jonathan.beezley@kitware.com>
2016-05-09 14:06:47 -0400

    Importing annotation code from histomicstk

--- plugin.cmake
+++ plugin.cmake
@@ -31,6 +31,11 @@ add_eslint_test(
   js_static_analysis_large_image_source
   "${CMAKE_CURRENT_LIST_DIR}/web_client"
 )
+add_eslint_test(
+  js_static_analysis_large_image_tests
+  "${CMAKE_CURRENT_LIST_DIR}/plugin_tests/client"
+  ESLINT_CONFIG_FILE "${CMAKE_CURRENT_LIST_DIR}/plugin_tests/client/.eslintrc"
+)
 
 add_python_test(tiles PLUGIN large_image BIND_SERVER EXTERNAL_DATA
   "sample_image.ptif" "plugins/large_image/sample_image.ptif"
@@ -48,4 +53,4 @@ add_python_test(girderless PLUGIN large_image BIND_SERVER EXTERNAL_DATA
 set_property(TEST server_large_image.girderless APPEND PROPERTY ENVIRONMENT
   "LARGE_IMAGE_DATA=${PROJECT_BINARY_DIR}/data/plugins/large_image")
 
-add_web_client_test(example "${CMAKE_CURRENT_LIST_DIR}/plugin_tests/exampleSpec.js" PLUGIN large_image)
+add_web_client_test(annotation "${CMAKE_CURRENT_LIST_DIR}/plugin_tests/client/annotation.js" PLUGIN large_image)
